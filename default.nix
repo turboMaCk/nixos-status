c360940d57e0c28fc07f19b4068bad8d0f747ec0
Marek Fajkus <marek.faj@gmail.com>
2019-12-23 19:57:31 +0100

    nixify

--- default.nix
+++ default.nix
@@ -3,6 +3,19 @@
 { nixpkgs ? (fetchTarball https://github.com/NixOS/nixpkgs/archive/f3293015015a907dc116981eb1ee0edd0410c5f9.tar.gz)
 }:
 with (import nixpkgs {});
-mkShell {
-  buildInputs = with elmPackages; [ elm elm-format elm2nix ];
+let
+  # Latest version of elm2nix doesn't support Elm 0.19.1
+  # using master as of 2019-12-23
+  # issue: https://github.com/hercules-ci/elm2nix/issues/36
+  elm2nix-master = fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "elm2nix";
+    sha256 = "0ing539di3a3j8ynplmq520256rr9fwmf9acz1z49b1501kxhw56";
+    rev = "38b61e45a860d7512f2ec2e458cf01c688835ddf";
+  };
+in {
+  shell = mkShell {
+    buildInputs = with elmPackages; [ elm elm-format elm2nix-master ];
+  };
+  html = import ./nix/nixos-status.nix { inherit nixpkgs; };
 }
---
+++ nix/elm-srcs.nix
@@ -0,0 +1,97 @@
+{
+
+      "krisajenkins/remotedata" = {
+        sha256 = "0m5bk0qhsjv14vajqrkph386696pnhj5rn51kgma8lwyvvx9ihw1";
+        version = "6.0.1";
+      };
+
+      "elm-explorations/markdown" = {
+        sha256 = "0k3110ixa4wwf3vkkdplagwah9ypr965qxr1y147rnsc1xsxmr6y";
+        version = "1.0.0";
+      };
+
+      "elm/json" = {
+        sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+        version = "1.1.3";
+      };
+
+      "elm/html" = {
+        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+        version = "1.0.0";
+      };
+
+      "elm/browser" = {
+        sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+        version = "1.0.2";
+      };
+
+      "elm-community/json-extra" = {
+        sha256 = "0hwlhdlvd5xi866m9bpnabcg2qrgcvfj34p1x1mhx85ymr3gv9mv";
+        version = "4.2.0";
+      };
+
+      "rtfeldman/elm-css" = {
+        sha256 = "05sgz21jnnmz6m5s74515337n1aanab2zzn8jpw3bhdjc9cwwfcj";
+        version = "16.0.1";
+      };
+
+      "elm/core" = {
+        sha256 = "0gyk7lx3b6vx2jlfbxdsb4xffn0wdvg5yxldq50jr2kk5dzc2prj";
+        version = "1.0.4";
+      };
+
+      "elm/url" = {
+        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+        version = "1.0.0";
+      };
+
+      "elm/http" = {
+        sha256 = "008bs76mnp48b4dw8qwjj4fyvzbxvlrl4xpa2qh1gg2kfwyw56v1";
+        version = "2.0.0";
+      };
+
+      "BrianHicks/elm-css-reset" = {
+        sha256 = "1cbdhm9pmzrb6ch2wfrxqma5h6cska557abg85bbdk4m88fy7ry9";
+        version = "1.0.1";
+      };
+
+      "elm/bytes" = {
+        sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
+        version = "1.0.8";
+      };
+
+      "elm/file" = {
+        sha256 = "1rljcb41dl97myidyjih2yliyzddkr2m7n74x7gg46rcw4jl0ny8";
+        version = "1.0.5";
+      };
+
+      "rtfeldman/elm-hex" = {
+        sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+        version = "1.0.0";
+      };
+
+      "elm/parser" = {
+        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+        version = "1.1.0";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "Skinney/murmur3" = {
+        sha256 = "1iiia68w8wlp2qjilyqgnzgjcxby47gla3rw5minj8i5c64zk18j";
+        version = "2.0.8";
+      };
+
+      "elm/virtual-dom" = {
+        sha256 = "0q1v5gi4g336bzz1lgwpn5b1639lrn63d8y6k6pimcyismp2i1yg";
+        version = "1.0.2";
+      };
+
+      "rtfeldman/elm-iso8601-date-strings" = {
+        sha256 = "1fa5sslklldy0dq8bm0zdkb9ni50yxhb09xb6lgk00x55bmza9ik";
+        version = "1.1.3";
+      };
+}
